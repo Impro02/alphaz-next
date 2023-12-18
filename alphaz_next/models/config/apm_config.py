3dae67c8e3661331f54042a0ca8886160028e3ff
Maxime MARTIN <maxime.martin@st.com>
2023-12-18 11:36:41 +0100

    feat: add environment in apm_config

--- alphaz_next/models/config/apm_config.py
+++ alphaz_next/models/config/apm_config.py
@@ -9,12 +9,19 @@ class ApmConfig(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     server_url: Optional[str] = Field(default=None)
+    environment: Optional[str] = Field(default=None)
     debug: bool = Field(default=True)
     active: bool = Field(default=False)
 
     @model_validator(mode="after")
     def validate_model(self):
-        if self.active and self.server_url is None:
+        if not self.active:
+            return self
+
+        if self.server_url is None:
             raise ValueError(f"server_url cannot be None if {self.active=}")
+
+        if self.environment is None:
+            raise ValueError(f"environment cannot be None if {self.active=}")
 
         return self
