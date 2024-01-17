9b76ceca3739b41dfc4c30b642c341cf462bb9c6
Maxime MARTIN <maxime.martin@st.com>
2024-01-17 11:39:18 +0100

    feat: use new formatter for logger

--- alphaz_next/utils/logger.py
+++ alphaz_next/utils/logger.py
@@ -1,10 +1,27 @@
 # MODULES
+import datetime
+from enum import Enum
+import os
+import re
 import sys
+import inspect
 import logging
 import unidecode
 from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
+
+DEFAULT_FORMAT = (
+    "{$date} - {$level:7} - {$pid:5} - {$file:>15}.{$line:<4} - {$name:<14}: $message"
+)
+DEFAULT_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+
+class LevelEnum(Enum):
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
 
 
 class AlphaLogger:
@@ -17,7 +34,14 @@ class AlphaLogger:
         interval: int = 1,
         backup_count: int = 10,
         file_name: Optional[str] = None,
+        logging_formatter: str = DEFAULT_FORMAT,
+        date_formatter: str = DEFAULT_DATE_FORMAT,
     ):
+        self._name = name
+        self._logging_formatter = logging_formatter
+        self._date_formatter = date_formatter
+        self._date_str = None
+
         if file_name is None:
             file_name = name
 
@@ -28,9 +52,7 @@ class AlphaLogger:
         directory_path = Path(directory)
         directory_path.mkdir(parents=True, exist_ok=True)
 
-        formatter = logging.Formatter(
-            "%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
-        )
+        formatter = logging.Formatter()
 
         logger_config = {
             "level": level,
@@ -66,57 +88,181 @@ class AlphaLogger:
     def info(
         self,
         message: str,
-        exc_info=None,
+        exc_info: Exception = None,
+        stack_level: int = 1,
         monitor: Optional[str] = None,
     ):
-        self._logger.info(message, exc_info=exc_info)
-        if monitor is not None:
-            self._monitoring_logger.info(
-                self._process_monitoring_message(message=message, monitor=monitor),
-                exc_info=exc_info,
-            )
+        self._log(
+            level=LevelEnum.INFO,
+            message=message,
+            stack=inspect.stack(),
+            exc_info=exc_info,
+            stack_level=stack_level,
+            monitor=monitor,
+        )
 
     def warning(
         self,
-        message,
-        exc_info=None,
+        message: str,
+        exc_info: Exception = None,
+        stack_level: int = 1,
         monitor: Optional[str] = None,
     ):
-        self._logger.warning(message, exc_info=exc_info)
-        self._warning_logger.warning(message, exc_info=exc_info)
-        if monitor is not None:
-            self._monitoring_logger.info(
-                self._process_monitoring_message(message=message, monitor=monitor),
-                exc_info=exc_info,
-            )
+        self._log(
+            level=LevelEnum.WARNING,
+            message=message,
+            stack=inspect.stack(),
+            exc_info=exc_info,
+            stack_level=stack_level,
+            monitor=monitor,
+        )
 
     def error(
         self,
-        message,
-        exc_info=None,
+        message: str,
+        exc_info: Exception = None,
+        stack_level: int = 1,
         monitor: Optional[str] = None,
     ):
-        self._logger.error(message, exc_info=exc_info)
-        self._error_logger.error(message, exc_info=exc_info)
-        if monitor is not None:
-            self._monitoring_logger.info(
-                self._process_monitoring_message(message=message, monitor=monitor),
-                exc_info=exc_info,
-            )
+        self._log(
+            level=LevelEnum.ERROR,
+            message=message,
+            stack=inspect.stack(),
+            exc_info=exc_info,
+            stack_level=stack_level,
+            monitor=monitor,
+        )
 
     def critical(
         self,
-        message,
-        exc_info=None,
+        message: str,
+        exc_info: Exception = None,
+        stack_level: int = 1,
         monitor: Optional[str] = None,
     ):
-        self._logger.critical(message, exc_info=exc_info)
-        self._error_logger.critical(message, exc_info=exc_info)
+        self._log(
+            level=LevelEnum.CRITICAL,
+            message=message,
+            stack=inspect.stack(),
+            exc_info=exc_info,
+            stack_level=stack_level,
+            monitor=monitor,
+        )
+
+    def _log(
+        self,
+        level: LevelEnum,
+        message: str,
+        stack: List[inspect.FrameInfo],
+        exc_info=None,
+        stack_level: int = 1,
+        monitor: Optional[str] = None,
+    ):
+        self._date_str = self._get_current_date()
+
+        message_formatted = self._get_formatted_message(
+            message=message,
+            level=level,
+            stack=stack,
+            stack_level=stack_level,
+        )
+
+        match level:
+            case LevelEnum.INFO:
+                self._logger.info(
+                    message_formatted,
+                    exc_info=exc_info,
+                    stacklevel=stack_level,
+                )
+            case LevelEnum.WARNING:
+                self._logger.warning(
+                    message_formatted,
+                    exc_info=exc_info,
+                    stacklevel=stack_level,
+                )
+                self._warning_logger.warning(
+                    message_formatted,
+                    exc_info=exc_info,
+                    stacklevel=stack_level,
+                )
+            case LevelEnum.ERROR:
+                self._logger.error(
+                    message_formatted,
+                    exc_info=exc_info,
+                    stacklevel=stack_level,
+                )
+                self._error_logger.error(
+                    message_formatted,
+                    exc_info=exc_info,
+                    stacklevel=stack_level,
+                )
+            case LevelEnum.CRITICAL:
+                self._logger.critical(
+                    message_formatted,
+                    exc_info=exc_info,
+                    stacklevel=stack_level,
+                )
+                self._error_logger.critical(
+                    message_formatted,
+                    exc_info=exc_info,
+                    stacklevel=stack_level,
+                )
+
         if monitor is not None:
-            self._monitoring_logger.info(
-                self._process_monitoring_message(message=message, monitor=monitor),
-                exc_info=exc_info,
+            message_formatted = self._process_monitoring_message(
+                message=message_formatted,
+                monitor=monitor,
             )
+            self._monitoring_logger.info(
+                message_formatted,
+                exc_info=exc_info,
+                stacklevel=stack_level,
+            )
+
+    def _get_formatted_message(
+        self,
+        message: str,
+        level: LevelEnum,
+        stack: List[inspect.FrameInfo],
+        stack_level: int = 1,
+    ):
+        message_formatted = self._logging_formatter
+
+        parameters = re.findall("\{\$([a-zA-Z0-9]*):?[0-9<>]*\}", message_formatted)
+
+        parameters_values = []
+
+        if stack_level >= len(stack):
+            stack_level = len(stack) - 1
+        caller = inspect.getframeinfo(stack[stack_level][0])
+
+        structure = "$%s"
+        keys = {
+            "date": self._date_str,
+            "pid": os.getpid(),
+            "level": level.value.upper(),
+            "name": self._name,
+            "path": caller.filename,
+            "file": caller.filename.split(os.sep)[-1].replace(".py", ""),
+            "line": caller.lineno,
+        }
+
+        for parameter_name in parameters:
+            if parameter_name in keys:
+                message_formatted = message_formatted.replace(
+                    structure % parameter_name, ""
+                )
+                parameters_values.append(keys[parameter_name])
+
+        message_formatted = message_formatted.format(*parameters_values).replace(
+            structure % "message", str(message)
+        )
+
+        return message_formatted
+
+    def _get_current_date(self):
+        current_date = datetime.datetime.now()
+        return current_date.strftime(self._date_formatter)
 
     @classmethod
     def _process_monitoring_message(cls, message: str, monitor: str) -> str:
