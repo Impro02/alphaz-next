6584fbf3fce5a3b1bbe75ae8a6c2429df3e5c134
Maxime MARTIN <maxime.martin@st.com>
2024-01-22 15:01:15 +0100

    feat: add tests

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -7,7 +7,9 @@ from unittest import TestCase
 # ALPHAZ_NEXT
 from alphaz_next.utils.logger import AlphaLogger
 
-LOG_PATTERN = r"\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\s-\sINFO\s-\s\d+\s-\s\w+\.\d+\s-\s\w+\s+:\s+.+"
+LOG_PATTERN = (
+    r"\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\s-\s\w+\s-\s\d+\s-\s\w+\.\d+\s-\s\w+:\s.+"
+)
 
 
 class TestAlphaLogger(TestCase):
@@ -39,6 +41,102 @@ class TestAlphaLogger(TestCase):
             rows = f.readlines()
 
         # THEN
+        self.assertGreater(len(rows), 0)
         for row in rows:
+            row = re.sub(r"\s+", " ", row).strip()
+            self.assertRegex(row, expected_log_pattern)
+
+    def test_error(self):
+        # GIVEN
+        expected_log_pattern = LOG_PATTERN
+
+        logger = AlphaLogger(
+            name="test",
+            directory=str(self._directory),
+        )
+
+        # WHEN
+        logger.error(message="Test error")
+
+        with open(Path(self._directory) / "test.log") as f:
+            rows_tests = f.readlines()
+
+        with open(Path(self._directory) / "errors.log") as f:
+            rows_errors = f.readlines()
+
+        # THEN
+        self.assertGreater(len(rows_tests), 0)
+        self.assertGreater(len(rows_errors), 0)
+        for row in rows_tests:
+            row = re.sub(r"\s+", " ", row).strip()
+            self.assertRegex(row, expected_log_pattern)
+
+        for row in rows_errors:
+            row = re.sub(r"\s+", " ", row).strip()
+            self.assertRegex(row, expected_log_pattern)
+
+    def test_warning(self):
+        # GIVEN
+        expected_log_pattern = LOG_PATTERN
+
+        logger = AlphaLogger(
+            name="test",
+            directory=str(self._directory),
+        )
+
+        # WHEN
+        logger.warning(message="Test warning")
+
+        with open(Path(self._directory) / "test.log") as f:
+            rows_tests = f.readlines()
+
+        with open(Path(self._directory) / "warnings.log") as f:
+            rows_warnings = f.readlines()
+
+        # THEN
+        self.assertGreater(len(rows_tests), 0)
+        self.assertGreater(len(rows_warnings), 0)
+        for row in rows_tests:
+            row = re.sub(r"\s+", " ", row).strip()
+            self.assertRegex(row, expected_log_pattern)
+
+        for row in rows_warnings:
+            row = re.sub(r"\s+", " ", row).strip()
+            self.assertRegex(row, expected_log_pattern)
+
+    def test_monitor(self):
+        # GIVEN
+        expected_log_pattern = LOG_PATTERN
+
+        logger = AlphaLogger(
+            name="test",
+            directory=str(self._directory),
+        )
+
+        # WHEN
+        logger.critical(message="Test monitor", monitor="monitor")
+
+        with open(Path(self._directory) / "test.log") as f:
+            rows_tests = f.readlines()
+
+        with open(Path(self._directory) / "errors.log") as f:
+            rows_warnings = f.readlines()
+
+        with open(Path(self._directory) / "monitoring.log") as f:
+            rows_monitor = f.readlines()
+
+        # THEN
+        self.assertGreater(len(rows_tests), 0)
+        self.assertGreater(len(rows_warnings), 0)
+        self.assertGreater(len(rows_monitor), 0)
+        for row in rows_tests:
+            row = re.sub(r"\s+", " ", row).strip()
+            self.assertRegex(row, expected_log_pattern)
+
+        for row in rows_warnings:
+            row = re.sub(r"\s+", " ", row).strip()
+            self.assertRegex(row, expected_log_pattern)
+
+        for row in rows_monitor:
             row = re.sub(r"\s+", " ", row).strip()
             self.assertRegex(row, expected_log_pattern)
