4c0e020834837a7a236726ad7ecdae22b5fee5b7
Maxime MARTIN <maxime.martin@st.com>
2024-04-26 10:40:17 +0200

    fix: rename Database as DataBase

--- alphaz_next/__init__.py
+++ alphaz_next/__init__.py
@@ -1,4 +1,4 @@
-from alphaz_next.utils.database import DataBase as Database
+from alphaz_next.utils.database import DataBase as DataBase
 from pysql_repo import (
     Repository as Repository,
     Service as Service,
